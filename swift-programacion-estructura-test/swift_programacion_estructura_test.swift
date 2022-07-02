9ca5b723d7e4adf9b950ebe643e925ade0da90e2
Ronaldo Andre <ronaldovh99@gmail.com>
2022-07-02 15:08:20 -0500

    Se agrego pruebas unitarias de Test Se agrego archivo utilitarios para el ejemplo

---
+++ swift-programacion-estructura-test/swift_programacion_estructura_test.swift
@@ -0,0 +1,32 @@
+//
+//  swift_programacion_estructura_test.swift
+//  swift-programacion-estructura-test
+//
+//  Created by Ronaldo Andre Vargas Huaman on 2/07/22.
+//
+
+import XCTest
+import swift_programacion_estructurada
+
+class swift_programacion_estructura_test: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        let suma = calcularSuma(a: 10, b: 20)
+        XCTAssertTrue(suma == 30)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
--- swift-programacion-estructurada/Utilitarios.swift
+++ swift-programacion-estructurada/Utilitarios.swift
@@ -1,5 +1,5 @@
 //
-//  main.swift
+//  Utilitarios.swift
 //  swift-programacion-estructurada
 //
 //  Created by Ronaldo Andre Vargas Huaman on 2/07/22.
@@ -7,8 +7,6 @@
 
 import Foundation
 
-print("Hello, World!")
-
-let suma = calcularSuma(a: 10, b: 20)
-
-print(suma)
+public func calcularSuma(a: Int, b: Int) -> Int {
+    return a + b
+}
