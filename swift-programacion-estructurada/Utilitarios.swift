bd184f319e6dfc9dca7f7929b5967c4b780ecf58
Ronaldo Andre <ronaldovh99@gmail.com>
2022-07-07 17:12:39 -0500

    Ejercicio 22 Agregando Test

--- swift-programacion-estructurada/Utilitarios.swift
+++ swift-programacion-estructurada/Utilitarios.swift
@@ -7,7 +7,31 @@
 
 import Foundation
 
-public func calcularSuma(a: Int, b: Int) -> Int {
-    return a + b
+public func calcularOperacionAritmeticas(operacion: Int, primerNumero:Int, segundoNumero:Int) {
+    switch(operacion){
+    case 1:
+        let suma = primerNumero + segundoNumero
+        print("La suma es: \(suma)")
+        break
+    case 2:
+        let resta = primerNumero - segundoNumero
+        print("La resta es: \(resta)")
+        break
+    case 3:
+        let multiplicacion = primerNumero * segundoNumero
+        print("La multiplicacion es: \(multiplicacion)")
+        break
+    case 4:
+        let division = primerNumero / segundoNumero
+        print("La division es: \(division)")
+        break
+    case 5:
+        let potencia = (primerNumero * primerNumero) + (segundoNumero * segundoNumero)
+        print("La potencia es: \(potencia)")
+        break
+    default:
+        print("Error...")
+        break
+    }
 }
 
--- swift-programacion-estructurada/main.swift
+++ swift-programacion-estructurada/main.swift
@@ -7,8 +7,26 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Programa que realize operaciones aritmeticas")
+print("|------------------------------------|")
+print("|               MENU                 |")
+print("|             1)SUMA                 |")
+print("|             2)RESTA                |")
+print("|             3)MULTIPLICACION       |")
+print("|             4)DIVISION             |")
+print("|             5)POTENCIA             |")
+print("|____________________________________|")
 
-let suma = calcularSuma(a: 10, b: 20)
-
-print(suma)
+print("Ingrese numero: ")
+private let operacion = Int(readLine()!)!
+if(operacion > 0 && operacion <= 5){
+    print("Ingrese primer numero: ")
+    let primerNumero = Int(readLine()!)!
+    print("Ingrese segundo numero: ")
+    let segundoNumero = Int(readLine()!)!
+    
+    calcularOperacionAritmeticas(operacion: operacion, primerNumero: primerNumero, segundoNumero: segundoNumero)
+    
+} else {
+    print("Error...")
+}
