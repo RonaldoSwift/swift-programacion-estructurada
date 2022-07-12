d0286062cb5d24db72bef318367e753e5b212b3c
Ronaldo Andre <ronaldovh99@gmail.com>
2022-07-12 17:37:21 -0500

    Ejercicio 38 agregando funciones

--- swift-programacion-estructurada/Utilitarios.swift
+++ swift-programacion-estructurada/Utilitarios.swift
@@ -7,7 +7,30 @@
 
 import Foundation
 
-public func calcularSuma(a: Int, b: Int) -> Int {
-    return a + b
+public func calcularSumaDeUnVectorPorFila() {
+    var s = 0
+    let numeroDeFilas = 3
+    let numeroDeColumnas = 3
+    var matriz = [
+        [-1, -1, -1],
+        [-1, -1, -1],
+        [-1, -1, -1]
+    ]
+    for i in 0...numeroDeFilas - 1 {
+        for j in 0...numeroDeColumnas - 1 {
+            print("dame dato: \(i),\(j)")
+            matriz[i][j] = Int(readLine()!)!
+        }
+    }
+    for i in 0...numeroDeFilas - 1 {
+        s = 0
+        for j in 0...numeroDeColumnas - 1 {
+            s += matriz[i][j]
+        }
+        for j in 0...numeroDeColumnas - 1 {
+            print(matriz[i][j])
+        }
+        print("la suma de este reglon es:, \(s)")
+        print("SALTO DE LINEA")
+    }
 }
-
--- swift-programacion-estructurada/main.swift
+++ swift-programacion-estructurada/main.swift
@@ -7,8 +7,5 @@
 
 import Foundation
 
-print("Hello, World!")
-
-let suma = calcularSuma(a: 10, b: 20)
-
-print(suma)
+print("programa que capture con número enteros una matriz de 10 X 10 y al final de cada renglón imprima la suma de sus elementos")
+calcularSumaDeUnVectorPorFila()
